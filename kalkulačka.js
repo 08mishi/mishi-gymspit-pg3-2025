0a004c4104f0d4b42b72fb1a7dba02d7b368364c
08mishi <michaela.samcenkova@student.gymspit.cz>
2025-12-23 12:33:59 +0100

    Refactor calculator program for clarity

--- kalkulačka.js
+++ kalkulačka.js
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Jednoduchá kalkulačka");
+        Console.WriteLine("kalkulačka");
 
         while (true)
         {
@@ -67,3 +67,4 @@ class Program
         }
     }
 }
+
