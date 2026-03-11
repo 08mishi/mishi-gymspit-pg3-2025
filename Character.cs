2a15d6ce1306aaabfc793c3b9d66a80c28e6eba2
08mishi <michaela.samcenkova@student.gymspit.cz>
2026-03-11 17:09:26 +0100

    Updated Character.cs with complete implementation including armorBonus, attackComboBonus, and all features.

--- Character.cs
+++ Character.cs
@@ -4,31 +4,67 @@ namespace GymSpit
 {
     public class Character
     {
+        public string Name { get; set; }
         public int HitPoints { get; set; }
         public int MaxHitPoints { get; set; }
-        public string Name { get; set; }
         public int Armor { get; set; }
+        public int Agility { get; set; }
         public bool Alive { get; set; }
 
-        public Character(string name, int maxHitPoints, int armor)
+        // VELKÁ FUNKCE 1: ArmorBonus - zvyšuje se při defense, snižuje se při útoku
+        public int ArmorBonus { get; set; }
+
+        // VELKÁ FUNKCE 2: AttackComboBonus - zvyšuje se při úspěšném útoku, resetuje se na miss
+        public int AttackComboBonus { get; set; }
+
+        public Character(string name, int maxHitPoints, int armor, int agility)
         {
             Name = name;
             MaxHitPoints = maxHitPoints;
             HitPoints = maxHitPoints;
             Armor = armor;
+            Agility = agility;
             Alive = true;
+            ArmorBonus = 0;
+            AttackComboBonus = 0;
         }
 
         public void Reset()
         {
             HitPoints = MaxHitPoints;
             Alive = true;
+            ArmorBonus = 0;
+            AttackComboBonus = 0;
         }
 
-        public void TakeTurn()
+        public int GetTotalArmor()
         {
-            // Logic for taking a turn in the game goes here.
-            Console.WriteLine(Name + " takes their turn.");
+            return Armor + ArmorBonus;
+        }
+
+        public int GetAttackBonus()
+        {
+            return AttackComboBonus;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            HitPoints -= damage;
+            if (HitPoints < 0) HitPoints = 0;
+            if (HitPoints == 0) Alive = false;
+        }
+
+        public void Heal(int amount)
+        {
+            HitPoints += amount;
+            if (HitPoints > MaxHitPoints) HitPoints = MaxHitPoints;
+        }
+
+        public bool TryDodge()
+        {
+            Random rand = new Random();
+            int dodgeRoll = rand.Next(1, 21);
+            return dodgeRoll > Agility;
         }
     }
 }
